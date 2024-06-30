a34ee978edd7ef0c46a6b841373f9055c4c8b162
Arsenic <reeeeee197@gmail.com>
2024-06-30 02:03:03 -0400

    Added new tag to tags.js

--- server_scripts/tags.js
+++ server_scripts/tags.js
@@ -1,4 +1,4 @@
-const tags = [
+const tags1 = [
     'minecraft:grass_block',
     'immersive_weathering:rooted_grass_block',
     'immersive_weathering:grassy_earthen_clay',
@@ -7,6 +7,16 @@ const tags = [
     'immersive_weathering:grassy_permafrost'
 ];
 
+const tags2 = [
+    'minecraft:cobblestone',
+    'minecraft:oak_log'
+];
+
 ServerEvents.tags('item', function(event) {
-    event.add('forge:grasses', ...tags);
+    for (let i = 0; i < tags1.length; i++) {
+        event.add('forge:grasses', tags1[i]);
+    }
+    for (let i = 0; i < tags2.length; i++) {
+        event.add('forge:tag2', tags2[i]);
+    }
 });
