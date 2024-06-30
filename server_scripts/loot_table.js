e2ef494c7cacd8079396ca267fd2043e5b0d1053
Arsenic <reeeeee197@gmail.com>
2024-06-30 05:51:14 -0400

    Made code cleaner

--- server_scripts/loot_table.js
+++ server_scripts/loot_table.js
@@ -1,21 +1,16 @@
-const addEntityLootModifier = (event, entities, lootToRemove) => {
-    if (!Array.isArray(entities)) {
-        entities = [entities];
-    }
-
-    entities.forEach(entity => {
-        const modifier = event.addEntityLootModifier(entity);
-        lootToRemove.forEach(loot => {
-            modifier.removeLoot(loot);
-        });
-    });
-};
-
 LootJS.modifiers((event) => {
-    addEntityLootModifier(event, ["minecraft:evoker", "minecraft:vindicator"], ["minecraft:emerald"]);
-    addEntityLootModifier(event, "minecraft:zombified_piglin", ["minecraft:gold_nugget", "minecraft:gold_ingot"]);
-    addEntityLootModifier(event, "minecraft:drowned", ['minecraft:copper_ingot']);
-    addEntityLootModifier(event, ["minecraft:zombie", "minecraft:zombie_villager", "minecraft:husk", "minecraft:iron_golem",], ["minecraft:iron_ingot"]);
-    addEntityLootModifier(event, "minecraft:wither_skeleton", ["minecraft:coal"]);
-    addEntityLootModifier(event, "minecraft:witch", ["minecraft:redstone"]);
+    const entitiesAndLoot = [
+        [["minecraft:evoker", "minecraft:vindicator"], ["minecraft:emerald"]],
+        ["minecraft:zombified_piglin", ["minecraft:gold_nugget", "minecraft:gold_ingot"]],
+        ["minecraft:drowned", ['minecraft:copper_ingot']],
+        [["minecraft:zombie", "minecraft:zombie_villager", "minecraft:husk", "minecraft:iron_golem"], ["minecraft:iron_ingot"]],
+        ["minecraft:wither_skeleton", ["minecraft:coal"]],
+        ["minecraft:witch", ["minecraft:redstone"]]
+    ];
+
+    entitiesAndLoot.forEach(([entities, loot]) => {
+        const entityList = Array.isArray(entities) ? entities : [entities];
+        const modifiers = entityList.map(entity => event.addEntityLootModifier(entity));
+        loot.forEach(lootItem => modifiers.forEach(modifier => modifier.removeLoot(lootItem)));
+    });
 });
