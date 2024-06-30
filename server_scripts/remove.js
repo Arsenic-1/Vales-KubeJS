c0694c90c85b22393eb1ed35b814e35e0a13cd0d
Arsenic <Arsenic@gmail.com>
2024-06-30 11:46:01 -0400

    removals.js merged with recipes.js

--- server_scripts/remove.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.recipes(event => {
-    event.remove({ id: 'moonsweaponry:diamond_greatsword' });
-    event.remove({ id: 'moonsweaponry:netherite_greatsword_smithing' });
-    event.remove({ id: 'quark:building/crafting/furnaces/cobblestone_furnace'});
-});
