fd4b3f9e959e548d33699c809ae5e0f0722726a1
Arsenic <reeeeee197@gmail.com>
2024-06-30 02:49:24 -0400

    denested and added a helper function to loot_table.js

--- server_scripts/loot_table.js
+++ server_scripts/loot_table.js
@@ -1,11 +1,18 @@
+const addEntityLootModifier = (event, entities, lootToRemove) => {
+    if (!Array.isArray(entities)) {
+        entities = [entities];
+    }
+
+    entities.forEach(entity => {
+        const modifier = event.addEntityLootModifier(entity);
+        lootToRemove.forEach(loot => {
+            modifier.removeLoot(loot);
+        });
+    });
+};
+
 LootJS.modifiers((event) => {
-    event
-        .addEntityLootModifier("minecraft:iron_golem")
-        .removeLoot("minecraft:iron_ingot");
-    event
-        .addEntityLootModifier("minecraft:evoker","minecraft:vindicator")
-        .removeLoot("minecraft:emerald");
-    event
-        .addEntityLootModifier("minecraft:zombified_piglin")
-        .removeLoot("minecraft:gold_nugget","minecraft:gold_ingot");
+    addEntityLootModifier(event, "minecraft:iron_golem", ["minecraft:iron_ingot"]);
+    addEntityLootModifier(event, ["minecraft:evoker", "minecraft:vindicator"], ["minecraft:emerald"]);
+    addEntityLootModifier(event, "minecraft:zombified_piglin", ["minecraft:gold_nugget", "minecraft:gold_ingot"]);
 });
