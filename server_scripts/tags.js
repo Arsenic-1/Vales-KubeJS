05a018003a520c5c694110f4e06bbdbd93fb2661
Arsenic <reeeeee197@gmail.com>
2024-06-30 01:50:45 -0400

    Changed tags.js

--- server_scripts/tags.js
+++ server_scripts/tags.js
@@ -1,26 +1,12 @@
-const tags = ServerEvents?.tags;
-const add = tags?.addItem?.bind(tags);
+const tags = [
+    'minecraft:grass_block',
+    'immersive_weathering:rooted_grass_block',
+    'immersive_weathering:grassy_earthen_clay',
+    'immersive_weathering:grassy_sandy_dirt',
+    'immersive_weathering:grassy_silt',
+    'immersive_weathering:grassy_permafrost'
+];
 
-if(add) {
-    add(
-        'item', 
-        'forge:grasses', 
-        'minecraft:grass_block',
-        'immersive_weathering:rooted_grass_block',
-        'immersive_weathering:grassy_earthen_clay',
-        'immersive_weathering:grassy_sandy_dirt',
-        'immersive_weathering:grassy_silt'
-    );
-} else {
-    console.error('add is null');
-}
-if(add) {
-    add(
-        'item', 
-        'forge:emeralds',  // Added comma here
-        'minecraft:emerald',
-        'minecraft:emerald_block'
-    );
-} else {
-    console.error('add is null');
-}
+ServerEvents.tags('item', function(event) {
+    event.add('forge:grasses', ...tags);
+});
