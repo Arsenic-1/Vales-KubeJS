a4202ed5653e8639259f38e6ccd2931620a122b2
Arsenic <reeeeee197@gmail.com>
2024-06-30 05:29:07 -0400

    Removed drops from: Witchs, Zombie Villagers, and Wither Skeleton. Moved Iron golem to the zombies line

--- server_scripts/loot_table.js
+++ server_scripts/loot_table.js
@@ -12,9 +12,10 @@ const addEntityLootModifier = (event, entities, lootToRemove) => {
 };
 
 LootJS.modifiers((event) => {
-    addEntityLootModifier(event, "minecraft:iron_golem", ["minecraft:iron_ingot"]);
     addEntityLootModifier(event, ["minecraft:evoker", "minecraft:vindicator"], ["minecraft:emerald"]);
     addEntityLootModifier(event, "minecraft:zombified_piglin", ["minecraft:gold_nugget", "minecraft:gold_ingot"]);
-    addEntityLootModifier(event, "minecraft:drowned", ['minecraft:copper_ingot'])
-    addEntityLootModifier(event, ["minecraft:zombie", "minecraft:husk"], ["minecraft:iron_ingot"])
+    addEntityLootModifier(event, "minecraft:drowned", ['minecraft:copper_ingot']);
+    addEntityLootModifier(event, ["minecraft:zombie", "minecraft:zombie_villager", "minecraft:husk", "minecraft:iron_golem",], ["minecraft:iron_ingot"]);
+    addEntityLootModifier(event, "minecraft:wither_skeleton", ["minecraft:coal"]);
+    addEntityLootModifier(event, "minecraft:witch", ["minecraft:redstone"]);
 });
