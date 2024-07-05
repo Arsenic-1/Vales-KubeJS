d0922d823bc8937e30b5793787fea3101cb5064a
Arsenic <Arsenic@gmail.com>
2024-07-04 23:22:27 -0400

    Improved code

--- server_scripts/recipes.js
+++ server_scripts/recipes.js
@@ -1,60 +1,55 @@
-const MOD = (domain, id, x) => `${x ? `${x}x ` : ""}${id.startsWith("#") ? "#" : ""}${domain}:${id.replace("#", "")}`
-const CR = (id, x) => MOD("create", id, x)
-const MC = (id, x) => MOD("minecraft", id, x)
-const FO = (id, x) => MOD("forge", id, x)
-const MW = (id, x) => MOD("moonsweaponry", id, x)
-
-function createRecipe(output, shape, mapping, template, itemToUpgrade, upgradeItem) {
-  return {
-    output,
-    shape,
-    mapping,
-    template,
-    itemToUpgrade,
-    upgradeItem
-  }
+// Mod shortcuts
+const MOD = (domain, id, x) => (x ? `${x}x ` : "") + (id.startsWith('#') ? '#' : "") + domain + ":" + id.replace('#', '');
+const CR = (id, x) => MOD("create", id, x);
+const MC = (id, x) => MOD("minecraft", id, x);
+const KJ = (id, x) => MOD("kubejs", id, x);
+const FD = (id, x) => MOD("farmersdelight", id, x);
+const SP = (id, x) => MOD("supplementaries", id, x);
+const FO = (id, x) => MOD("forge", id, x);
+const AC = (id, x) => MOD("aquaculture", id, x);
+const MW = (id, x) => MOD("moonsweaponry", id, x);
+// Recipe creation functions
+function createShapedRecipe(output, shape, mapping) {
+    return { 'output': output, 'shape': shape, 'mapping': mapping };
 }
-
-function registerRecipes(event, recipe) {
-  if (recipe.shape) {
-    event.shaped(recipe.output, recipe.shape, recipe.mapping)
-  }
-  if (recipe.template) {
-    event.smithing(recipe.output, recipe.template, recipe.itemToUpgrade, recipe.upgradeItem)
-  }
-  if (recipe.mapping) {
-    event.recipes.create.mechanical_crafting(recipe.output, recipe.shape, recipe.mapping)
-  }
+function createSmithingRecipe(output, template, itemToUpgrade, upgradeItem) {
+    return { 'output': output, 'template': template, 'itemToUpgrade': itemToUpgrade, 'upgradeItem': upgradeItem };
 }
-
-const recipes = [
-  createRecipe(MW("diamond_greatsword"), ["   ", " d ", "dsd"], { d: MC("diamond"), s: MC("stick") }),
-  createRecipe(MW("netherite_greatsword"), null, null, MC("echo_shard"), MW("diamond_greatsword"), MC("oak_log")),
-  createRecipe(MC("dirt"), [" GGG ", "GGGGG", "GGGGG", "GGGGG", " GGG "], { G: FO("#grasses") })
-]
-
-ServerEvents.recipes(event => {
-  event.remove({ id: MW("diamond_greatsword") })
-  event.remove({ id: MW("netherite_greatsword_smithing") })
-  event.remove({ id: CR("building/crafting/furnaces/cobblestone_furnace") })
-
-  recipes.forEach(recipe => registerRecipes(event, recipe))
-})
-
-BlockEvents.rightClicked(event => {
-  const blockRecipes = new Map()
-  blockRecipes.set(`${MC("cobblestone")}-${MC("redstone")}`, MC("redstone_block"))
-  blockRecipes.set(`${MC("sand")}-${MC("birch_planks")}`, MC("lava"))
-  blockRecipes.set(`${MC("red_sand")}-${MC("birch_planks")}`, MC("lava"))
-
-  const handleBlockAndItem = (event, newBlockId) => {
-    event.block.set(newBlockId)
-    event.item.shrink(1)
-  }
-
-  const key = `${event.block.id}-${event.item.id}`
-  const newBlockId = blockRecipes.get(key)
-  if (newBlockId) {
-    handleBlockAndItem(event, newBlockId)
-  }
-})
+function createMechanicalCraftingRecipe(output, shape, mapping) {
+    return { 'output': output, 'shape': shape, 'mapping': mapping };
+}
+// Recipe registration function
+function registerRecipes(event, shapedRecipe, smithingRecipe, mechanicalCraftingRecipe) {
+    event.shaped(shapedRecipe.output, shapedRecipe.shape, shapedRecipe.mapping);
+    event.smithing(smithingRecipe.output, smithingRecipe.template, smithingRecipe.itemToUpgrade, smithingRecipe.upgradeItem);
+    event.recipes.create.mechanical_crafting(mechanicalCraftingRecipe.output, mechanicalCraftingRecipe.shape, mechanicalCraftingRecipe.mapping);
+}
+// Recipe definitions
+const shapedRecipe = createShapedRecipe(MW('diamond_greatsword'), ['   ', ' d ', 'dsd'], { d: MC('diamsond'), s: MC('stick') });
+const smithingRecipe = createSmithingRecipe(MW('netherite_greatsword'), MC('echo_shard'), MW('diamond_greatsword'), MC('oak_log'));
+const mechanicalCraftingRecipe = createMechanicalCraftingRecipe(MC('dirt'), [' GGG ', 'GGGGG', 'GGGGG', 'GGGGG', ' GGG '], { G: FO('#grasses') });
+// Recipe removals and registration
+ServerEvents.recipes((event) => {
+    // Recipe removals
+    event.remove({ id: MW('diamond_greatsword') });
+    event.remove({ id: MW('netherite_greatsword_smithing') });
+    event.remove({ id: CR('building/crafting/furnaces/cobblestone_furnace') });
+    event.remove({ id: MC('furnace') });
+    // Register recipes
+    registerRecipes(event, shapedRecipe, smithingRecipe, mechanicalCraftingRecipe);
+});
+const blockRecipes = new Map();
+blockRecipes.set(`${MC('cobblestone')}-${MC('redstone')}`, MC('redstone_block'));
+blockRecipes.set(`${MC('sand')}-${MC('birch_planks')}`, MC('lava'));
+blockRecipes.set(`${MC('red_sand')}-${MC('birch_planks')}`, MC('lava'));
+BlockEvents.rightClicked((event) => {
+    const key = `${event.block.id}-${event.item.id}`;
+    const newBlockId = blockRecipes.get(key);
+    if (newBlockId) {
+        handleBlockAndItem(event, newBlockId);
+    }
+});
+function handleBlockAndItem(event, newBlockId) {
+    event.block.set(newBlockId);
+    event.item.shrink(1);
+}
